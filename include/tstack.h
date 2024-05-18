a7fc37a806ae19dd2e3c9456cea668e9646cab43
Jkalashnikova <151378645+Jkalashnikova@users.noreply.github.com>
2024-05-19 00:17:12 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,38 +1,37 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
-template<typename T, int size>
-class TStack {
+template <class T>
+class Tstack {
  private:
-    T arr[100];
-    int top;
+        T array[100];
+        int top;
 
  public:
-    TStack() :top(-1) {} 
-    void push(T value) {
-      if (isFull()) {
-        throw std::string("Full");
-      } else {
-        arr[++top] = value;
-      }
-    }
-    const T& pop() {
-      if (isEmpty()) {
-        throw std::string("Eempty");
-      } else {
-        return arr[top--];
-      }
-    }
-    bool isEmpty()const {
-      return top == -1;
-    }
-    bool isFull()const {
-      return top == size - 1;
-    }
-    const T& get()const {
-      return arr[top];
-    }
+        Tstack() :top(-1) {}
+        void push(T value) {
+            if (isFull())
+                throw std::string("Full!");
+            else
+                array[++top] = value;
+        }
+        const T& pop() {
+            if (isEmpty())
+                throw std::string("Empty!");
+            else
+                return array[top--];
+        }
+        const T& get() {
+            return array[top];
+        }
+        bool isFull() const {
+            return (top == 100);
+        }
+        bool isEmpty() const {
+            return (top == -1);
+        }
 };
 
 #endif  // INCLUDE_TSTACK_H_
