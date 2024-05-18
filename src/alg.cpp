c0d0e3749e8e09de088d64d0062729c2399f380d
Jkalashnikova <151378645+Jkalashnikova@users.noreply.github.com>
2024-05-19 00:18:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,76 +2,116 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-std::map<char, int> priority {
-        {'(', 0}, {')', 1}, {'-', 2}, {'+', 2}, {'*', 3}, {'/', 3}
-};
-TStack<char, 100> stack1;
-TStack<int, 100> stack2;
-std::string infx2pstfx(std::string inf) {
-    std::string c = "";
-    for (int i = 0; i < inf.length(); i++) {
-        if (inf[i] >= '0' && inf[i] <= '9') {
-            c += inf[i];
-            c += ' ';
-        } else {
-            if (inf[i] == '(' || stack1.isEmpty() == 1 ||
-            priority[inf[i]] > priority[stack1.get()]) {
-                stack1.push(inf[i]);
-            } else if (inf[i] == ')') {
-                while (stack1.get() != '(') {
-                    c = c + stack1.pop() + ' ';
-                }
-                if (stack1.get() == '(') {
-                    stack1.pop();
-                }
-            } else if (priority[inf[i]] <= priority[stack1.get()]) {
-                char item = stack1.pop();
-                c = c + item + ' ';
-                stack1.push(inf[i]);
-            }
-        }
-    }
-    while (stack1.isEmpty() != 1) {
-        c = c + stack1.pop();
-        if (stack1.isEmpty() != 1) {
-            c += ' ';
-        }
-    }
-    return c;
+
+int p(char ch) {
+    if (ch == '(') return 0;
+    else if (ch == ')') return 1;
+    else if (ch == '+' || ch == '-') return 2;
+    else if (ch == '*' || ch == '/') return 3;
+    else
+        return -1;
 }
-int eval(std::string pref) {
-    std::string str;
-    char cPerem;
-    for (char i : pref) {
-        if ((i >= '0' && i <= '9')) {
-            str += i;
-        } else if (!str.empty() && i == ' ') {
-            stack2.push(std::stoi(str));
-            str.clear();
+
+bool isNum(char ch) {
+    return (ch >= '0' && ch <= '9');
+}
+
+int len(std::string str) {
+    char ch = str[0];
+    int res = 0;
+    while (ch != '\0') {
+        ++res;
+        ch = str[res];
+    }
+    return res;
+}
+
+int convertToInt(char ch) {
+    char key[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+    int left = 0, right = sizeof(key) - 1;
+    int mid = 0;
+    while (left <= right) {
+        mid = (left + right) / 2;
+        if (ch > key[mid]) {
+            left = mid + 1;
+        } else if (ch < key[mid]) {
+            right = mid - 1;
         } else {
-            switch (i) {
-                case '-': {
-                    cPerem = stack2.pop();
-                    stack2.push(stack2.pop() - cPerem);
-                    break;
-                }
-                case '/': {
-                    cPerem = stack2.pop();
-                    stack2.push(stack2.pop() / cPerem);
-                    break;
-                }
-                case '+': {
-                    cPerem = stack2.pop();
-                    stack2.push(stack2.pop() + cPerem);
-                    break;
-                }
-                case '*': {
-                    cPerem = stack2.pop();
-                    stack2.push(stack2.pop() * cPerem);
-                    break;
-                }
-            }
+            return mid;
         }
     }
-    return stack2.pop();
+    return mid;
+}
+
+std::string infx2pstfx(std::string inf) {
+    Tstack<char> stch;
+    std::string result;
+    char tmp;
+    int i = 0;
+    char ch = inf[0];
+    while (ch != '\0') {
+        if (isNum(ch)) {
+            result = result + ch + " ";
+        } else {
+            if (ch == ')')  {
+                if (!stch.isEmpty()) {
+                    tmp = stch.pop();
+                    while (tmp != '(') {
+                        result = result + tmp + " ";
+                            if (!stch.isEmpty())
+                                tmp = stch.pop();
+                    }
+                }
+            } else if ((stch.isEmpty()) || ch == '(' || p(ch) > p(stch.get())) {
+                stch.push(ch);
+            } else if ((!stch.isEmpty())&&(p(ch) <= p(stch.get()))) {
+                while ((!stch.isEmpty())&&(p(ch) <= p(stch.get()))) {
+                    tmp = stch.pop();
+                    result = result + tmp + " ";
+                }
+                stch.push(ch);
+            }
+        }
+        ++i;
+        ch = inf[i];
+    }
+    if (!stch.isEmpty()) {
+        while (!stch.isEmpty()) {
+            tmp = stch.pop();
+            if (stch.isEmpty()) result = result + tmp;
+            else
+                result = result + tmp + " ";
+        }
+    }
+    return result;
+}
+
+int eval(std::string pref) {
+    Tstack<int> st_int;
+    int l = len(pref);
+    int tmp1 = 0, tmp2 = 0, res_tmp = 0, result = 0;
+    int i = 0;
+    char ch = pref[0];
+    while (ch != '\0') {
+        if (ch != ' ') {
+            if (isNum(ch)) {
+                st_int.push(convertToInt(ch));
+            } else {
+                if (!st_int.isEmpty()) tmp1 = st_int.pop();
+                if (!st_int.isEmpty()) tmp2 = st_int.pop();
+                if (ch == '+') res_tmp = tmp2 + tmp1;
+                else if (ch == '-') res_tmp = tmp2 - tmp1;
+                else if (ch == '*') res_tmp = tmp2 * tmp1;
+                else if (ch == '/') res_tmp = tmp2 / tmp1;
+                st_int.push(res_tmp);
+            }
+        }
+        ++i;
+        if (i <= l) {
+            ch = pref[i];
+        }
+    }
+    if (!st_int.isEmpty())
+        result = st_int.pop();
+    return result;
 }
